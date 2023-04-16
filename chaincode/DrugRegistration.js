2ae05ea658babb3d98160ece6ede9b31c47fd470
Ritam Das <ritam.das@walmart.com>
2023-04-16 13:11:08 +0530

    Initial Commit

---
+++ chaincode/DrugRegistration.js
@@ -0,0 +1,70 @@
+"use strict";
+const {Contract} = require("fabric-contract-api");
+
+class DrugRegistrationContract extends Contract {
+    constructor() {
+        super("org.pharma-network.drugRegistration");
+    }
+
+    async instantiate(ctx) {
+        console.log("Pharmanet Chaincode is Instantiated");
+    }
+
+    /**
+     * This transaction is used by any organisation registered as a ‘manufacturer’ to register a new drug on the ledger. 
+     * @param ctx - The transaction context object
+     * @param drugName - Name of the drug
+     * @param serialNo 
+     * @param mfgDate 
+     * @param expDate 
+     * @param companyCRN - unique company CRN
+     * @returns - drug object 
+     */
+
+    async addDrug(ctx, drugName, serialNo, mfgDate, expDate, companyCRN) {
+        //This transaction should be invoked only by a manufacturer registered on the ledger.
+        try {
+            if (ctx.clientIdentity.getMSPID() != "manufacturerMSP") {
+                return {
+                    error: "Only Manufacturer Org can add drugs on the pharma-network"
+                };
+            }
+            //composite key for storing drug
+            const productIDKey = ctx.stub.createCompositeKey(
+                "org.pharma-network.productIDKey",
+                [serialNo, drugName]
+            );
+            //fetching manufacturer org details from the ledger using partial composite key 
+            let manufacturerCompKey = await ctx.stub.getStateByPartialCompositeKey(
+                "org.pharma-network.companyId",
+                [companyCRN]
+            );
+            //manuKey hold the return object from the itterator manuKey.value.key hold the composite key of the Manufacturer org
+            let manuKey = await manufacturerCompKey.next();
+
+            let newDrugObj = {
+                productID: productIDKey,
+                name: drugName,
+                manufacturer: manuKey.value.key,
+                manufacturingDate: mfgDate,
+
+                expiryDate: expDate,
+                owner: manuKey.value.key,
+                shipment: "",
+            };
+
+            let dataBuffer = Buffer.from(JSON.stringify(newDrugObj));
+            //storing the new drug object on the ledger 
+            await ctx.stub.putState(productIDKey, dataBuffer);
+            return newDrugObj;
+
+        } catch (err) {
+            return {
+                error: "Unable to register Drug on the network, check input parameters",
+                errorTrace: err.toString()
+            };
+        }
+
+    }
+}
+module.exports = DrugRegistrationContract;
